db38f749d0edf6032c00fd068eb1ffd32d26cf9f
Vlad <karelinvlad.dev@gmail.com>
2023-09-26 12:22:35 +0300

    Delete useless classes; Erase AdminController, EmployeeController classes;

--- src/main/java/ru/karelin/project/controllers/AdminController.java
+++ src/main/java/ru/karelin/project/controllers/AdminController.java
@@ -1,38 +1,10 @@
 package ru.karelin.project.controllers;
 
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.karelin.project.models.Employee;
-import ru.karelin.project.models.EmployeeCredentials;
-import ru.karelin.project.models.Role;
-import ru.karelin.project.services.EmployeeCredentialsService;
-import ru.karelin.project.services.EmployeeService;
-import ru.karelin.project.validators.EmployeeCredentialsValidator;
-import ru.karelin.project.validators.EmployeeValidator;
-import ru.karelin.project.utility.PageConfig;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-    private final EmployeeCredentialsValidator empCredentialsValidator;
-    private final EmployeeValidator employeeValidator;
-    private final EmployeeCredentialsService empCredentialsService;
-    private final EmployeeService employeeService;
-
-    @Autowired
-    public AdminController(EmployeeCredentialsValidator empCredentialsValidator, EmployeeCredentialsService empCredentialsService, EmployeeValidator employeeValidator, EmployeeService employeeService) {
-        this.empCredentialsValidator = empCredentialsValidator;
-        this.empCredentialsService = empCredentialsService;
-        this.employeeValidator = employeeValidator;
-        this.employeeService = employeeService;
-    }
-
 
 }
--- src/main/java/ru/karelin/project/controllers/EmployeeController.java
+++ src/main/java/ru/karelin/project/controllers/EmployeeController.java
@@ -1,44 +1,10 @@
 package ru.karelin.project.controllers;
 
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.karelin.project.models.Employee;
-import ru.karelin.project.models.EmployeeCredentials;
-import ru.karelin.project.security.EmployeeDetails;
-import ru.karelin.project.services.EmployeeCredentialsService;
-import ru.karelin.project.services.EmployeeService;
-import ru.karelin.project.validators.EmployeeCredentialsValidator;
-import ru.karelin.project.validators.EmployeeValidator;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
-
-    private final EmployeeCredentialsService employeeCredentialsService;
-    private final EmployeeCredentialsValidator empCredentialsValidator;
-    private final EmployeeService employeeService;
-    private final EmployeeValidator employeeValidator;
-    private final PasswordEncoder passwordEncoder;
-
-    @Autowired
-    public EmployeeController(EmployeeCredentialsService employeeCredentialsService, PasswordEncoder passwordEncoder, EmployeeCredentialsValidator empCredentialsValidator, EmployeeService employeeService, EmployeeValidator employeeValidator) {
-        this.employeeCredentialsService = employeeCredentialsService;
-        this.passwordEncoder = passwordEncoder;
-        this.empCredentialsValidator = empCredentialsValidator;
-        this.employeeService = employeeService;
-        this.employeeValidator = employeeValidator;
-    }
 
 }
